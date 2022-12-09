9953c0dceed9a5a60aa7e0ab2700a62f4588e4f8
Yuting Wang <yuting_wang@progressive.com>
2022-12-09 16:02:02 -0500

    Day 05

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,6 +112,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
+        additionalLanguages: ['csharp'],
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
